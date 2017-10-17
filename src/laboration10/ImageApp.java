f435071a6859a4849e8483ead95fbe3fd6c4f4f5
Philip Ekholm <philipekholm@protonmail.com>
2017-10-17 15:32:34 +0200

    Lab10 updated, fixed imageChooser

--- src/laboration10/ImageApp.java
+++ src/laboration10/ImageApp.java
@@ -3,13 +3,17 @@ import javax.swing.*;
 
 public class ImageApp {
 	public void newFrame(JPanel panel, String title) {
+		final int OFFSET = 50;
+		
 		JFrame frame = new JFrame( title );
 		
 		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 		frame.add( panel );
 		frame.pack();
 		frame.setVisible(true);
-		frame.setResizable(false);
+		frame.setLocation(OFFSET, OFFSET);
+		/* TODO: Change later to false */
+		frame.setResizable(true);
 	}
 	
 	public static void main(String[] args) {
--- src/laboration10/ImageChooser.java
+++ src/laboration10/ImageChooser.java
@@ -4,25 +4,25 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public final class ImageChooser extends JPanel{
-	/* Ser. Version def. ID required to avoid warning */
+	/* Serial Version default ID required to avoid warning */
 	private static final long serialVersionUID = 1L;
 
 	private JPanel pnlRadioButtons;
 	private JRadioButton[] rb;
+	private ButtonGroup radioGroup = new ButtonGroup();
 	private JButton btn;
 	private final Controller controller;
-	private final String[] imgPaths = {
-		"images/london06.jpg",
-		"images/filmlogga.jpg",
-		"images/lugi.gif",
-		"images/tandem1.jpg",
-		"images/program.bmp",
-	};
 	
 	public ImageChooser(Controller controller){
 		this.controller = controller;
+		final int 	WINDOW_WIDTH  = 250,
+					WINDOW_HEIGHT = 200;
+		
+		setLayout(null);
+		setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
 		
 		initializeComponents();
+		setBounds();
 		addListeners();
 		
 		add(pnlRadioButtons, BorderLayout.CENTER);
@@ -30,44 +30,64 @@ public final class ImageChooser extends JPanel{
 	}
 	
 	private void initializeComponents() {
+		
 		final int 	NBR_RADIO_BUTTONS = 5,
-					GRID_WIDTH = 5,
-					GRID_HEIGHT = 1;
+					GRID_WIDTH        = 5,
+					GRID_HEIGHT       = 1;
+		final String[] LABELS = {
+			"London",
+			"FilmLogga",
+			"Lugi",
+			"Tandem",
+			"Detta program",
+		};
 		
 		rb = new JRadioButton[NBR_RADIO_BUTTONS];
 		pnlRadioButtons = new JPanel(new GridLayout(GRID_WIDTH, GRID_HEIGHT));
-		btn = new JButton();
+		btn = new JButton("Ta bort bild");
 		
 		for (int i = 0; i < NBR_RADIO_BUTTONS; ++i) {
 			rb[i] = new JRadioButton();
 			pnlRadioButtons.add(rb[i]);
+			rb[i].setText(LABELS[i]);
+			radioGroup.add(rb[i]);
 		}
+	}
+	
+	private void setBounds() {
+		btn.setBounds(5, 170, 240, 25);
+		pnlRadioButtons.setBounds(10, 10, 150, 150);
 	}
 	
 	public void addListeners(){
-		ImageListener listener = new ImageListener();
+		final String IMGDIR = "images/";
+		final String[] IMGPATHS = {
+			"london06.jpg",
+			"filmlogga.jpg",
+			"lugi.gif",
+			"tandem1.jpg",
+			"program.bmp",
+		};
 
 		for (int i = 0; i < rb.length; ++i) {
-			rb[i].addActionListener(listener);
+			rb[i].addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					for (int i = 0; i < rb.length; ++i) {
+						if (rb[i].isSelected())
+							controller.newImage(IMGDIR + IMGPATHS[i]);
+					}
+				}
+			});
 		}
 		
-		btn.addActionListener( new EraseListener() );
-	}
-	
-	private final class EraseListener implements ActionListener{
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			controller.eraseImage();
-		}
-	}
-	
-	private final class ImageListener implements ActionListener{
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			for (int i = 0; i < rb.length; ++i) {
-				if (rb[i].isSelected())
-					controller.newImage(imgPaths[i]);
+		/* Erase chosen image */
+		
+		btn.addActionListener( new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				controller.eraseImage();
 			}
-		}
+		} );
 	}
 }
